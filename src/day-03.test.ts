7a436de6f85cf6291376538d33205bc7dd21a445
Jacques Cornat <cornat.jacques@gmail.com>
2024-12-09 23:26:32 +0100

    day 03 - part 1 OK - part 2 NOK

---
+++ src/day-03.test.ts
@@ -0,0 +1,85 @@
+import { assertEquals } from '@std/assert';
+import { solvePart1, solvePart2 } from './day-03.ts';
+
+Deno.test('Solve Part 1 - Simple', () => {
+  const testValue = [
+    'mul(2,4)',
+  ];
+
+  const res = solvePart1(testValue);
+  assertEquals(res, 8);
+});
+
+Deno.test('Solve Part 1 - Simple', () => {
+  const testValue = [
+    'mul(2,4)xmul(2,4)',
+  ];
+
+  const res = solvePart1(testValue);
+  assertEquals(res, 16);
+});
+
+Deno.test('Solve Part 1 - Simple', () => {
+  const testValue = [
+    'mul(2,4)xmul(2,4)',
+    'mul(2,4)xmul(2,4)',
+  ];
+
+  const res = solvePart1(testValue);
+  assertEquals(res, 32);
+});
+
+Deno.test('Solve Part 1', () => {
+  const testValue = [
+    'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))',
+  ];
+
+  const res = solvePart1(testValue);
+  assertEquals(res, 161);
+});
+
+Deno.test('Solve Part 2', () => {
+  const testValue = [
+    "don't()mul(5,5)",
+  ];
+
+  const res = solvePart2(testValue);
+  assertEquals(res, 0);
+});
+
+Deno.test('Solve Part 2', () => {
+  const testValue = [
+    'do()mul(5,5)',
+  ];
+
+  const res = solvePart2(testValue);
+  assertEquals(res, 25);
+});
+
+Deno.test('Solve Part 2', () => {
+  const testValue = [
+    "don't()mul(5,5)mul(5,5)do()mul(5,5)",
+  ];
+
+  const res = solvePart2(testValue);
+  assertEquals(res, 25);
+});
+
+Deno.test('Solve Part 2', () => {
+  const testValue = [
+    "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+  ];
+
+  const res = solvePart2(testValue);
+  assertEquals(res, 48);
+});
+
+Deno.test('Solve Part 2', () => {
+  const testValue = [
+    "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+    "don't()mul(5,5)mul(5,5)do()mul(5,5)",
+  ];
+
+  const res = solvePart2(testValue);
+  assertEquals(res, 48 + 25);
+});
---
+++ src/day-03.ts
@@ -0,0 +1,56 @@
+import { readLines } from './utils.ts';
+
+if (import.meta.main) {
+  init()
+    .catch(console.error);
+}
+
+async function init() {
+  const lines = await readLines(import.meta.filename!);
+  const part1 = solvePart1(lines);
+  const part2 = solvePart2(lines);
+
+  console.log(`Part 1 : ${part1}`);
+  console.log(`Part 2 : ${part2}`);
+}
+
+export function solvePart1(lines: string[]) {
+  return lines.reduce((parentAcc, line) => {
+    const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
+    const array = [...line.matchAll(regex)];
+    const reduce2 = array.reduce((childAcc, value) => {
+      const multiplication = +value[1] * +value[2];
+      return multiplication + childAcc;
+    }, 0);
+
+    return reduce2 + parentAcc;
+  }, 0);
+}
+
+export function solvePart2(lines: string[]) {
+  return lines.reduce((acc, line) => {
+    const regex = /(?:mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))/g;
+    const array = [...line.matchAll(regex)];
+    let enable = true;
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {
+      const value = array[i];
+      const command = value[0];
+      if (command === `don't()`) {
+        enable = false;
+        continue;
+      } else if (command === 'do()') {
+        enable = true;
+        continue;
+      }
+
+      if (!enable) {
+        continue;
+      }
+
+      total += +value[1] * +value[2];
+    }
+
+    return total + acc;
+  }, 0);
+}
