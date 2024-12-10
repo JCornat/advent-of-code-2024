6a80b95c54edabc1dee0f2c428baa287f382eb69
Jacques Cornat <cornat.jacques@gmail.com>
2024-12-10 22:40:26 +0100

    day 04 - part 1 completed

---
+++ src/day-04.test.ts
@@ -0,0 +1,167 @@
+import { assertEquals } from '@std/assert';
+import { addUpCoordinates, Coordinate, CoordinateGroup, getCharacter, hasXmasWord, isCoordinateValid, solvePart1, solvePart2 } from './day-04.ts';
+
+Deno.test('Solve Part 1 - Coordinate Valid', () => {
+  const matrix = [
+    '.',
+  ];
+
+  const coordinate = [0, 0] satisfies Coordinate;
+  const res = isCoordinateValid(coordinate, matrix);
+  assertEquals(res, true);
+});
+
+Deno.test('Solve Part 1 - Coordinate invalid - Width negative', () => {
+  const matrix = [
+    '.',
+  ];
+
+  const coordinate = [-1, 0] satisfies Coordinate;
+  const res = isCoordinateValid(coordinate, matrix);
+  assertEquals(res, false);
+});
+
+Deno.test('Solve Part 1 - Coordinate invalid - Height negative', () => {
+  const matrix = [
+    '.',
+  ];
+
+  const coordinate = [0, -1] satisfies Coordinate;
+  const res = isCoordinateValid(coordinate, matrix);
+  assertEquals(res, false);
+});
+
+Deno.test('Solve Part 1 - Coordinate invalid - Width & height negative', () => {
+  const matrix = [
+    '.',
+  ];
+
+  const coordinate = [-14, -18] satisfies Coordinate;
+  const res = isCoordinateValid(coordinate, matrix);
+  assertEquals(res, false);
+});
+
+Deno.test('Solve Part 1 - Coordinate invalid - Height too large', () => {
+  const matrix = [
+    '.',
+  ];
+
+  const coordinate = [0, 1] satisfies Coordinate;
+  const res = isCoordinateValid(coordinate, matrix);
+  assertEquals(res, false);
+});
+
+Deno.test('Solve Part 1 - Coordinate invalid - Width too large', () => {
+  const matrix = [
+    '.',
+  ];
+
+  const coordinate = [1, 0] satisfies Coordinate;
+  const res = isCoordinateValid(coordinate, matrix);
+  assertEquals(res, false);
+});
+
+Deno.test('Solve Part 1 - Coordinate valid - multiple lines', () => {
+  const matrix = [
+    '..',
+    '..',
+  ];
+
+  const coordinate = [1, 1] satisfies Coordinate;
+  const res = isCoordinateValid(coordinate, matrix);
+  assertEquals(res, true);
+});
+
+Deno.test('Solve Part 1 - addUpCoordinates', () => {
+  const coordinate1 = [0, 0] satisfies Coordinate;
+  const coordinate2 = [0, 0] satisfies Coordinate;
+  const res = addUpCoordinates(coordinate1, coordinate2);
+  assertEquals(res, [0, 0]);
+});
+
+Deno.test('Solve Part 1 - addUpCoordinates - positive', () => {
+  const coordinate1 = [0, 1] satisfies Coordinate;
+  const coordinate2 = [1, 0] satisfies Coordinate;
+  const res = addUpCoordinates(coordinate1, coordinate2);
+  assertEquals(res, [1, 1]);
+});
+
+Deno.test('Solve Part 1 - addUpCoordinates - negative', () => {
+  const coordinate1 = [2, 3] satisfies Coordinate;
+  const coordinate2 = [-5, -6] satisfies Coordinate;
+  const res = addUpCoordinates(coordinate1, coordinate2);
+  assertEquals(res, [-3, -3]);
+});
+
+Deno.test('Solve Part 1 - getCharacter - valid', () => {
+  const matrix = [
+    'AB',
+    'CD',
+  ];
+
+  const coordinate = [1, 1] satisfies Coordinate;
+  const res = getCharacter(coordinate, matrix);
+  assertEquals(res, 'D');
+});
+
+Deno.test('Solve Part 1 - getCharacter - invalid', () => {
+  const matrix = [
+    'AB',
+    'CD',
+  ];
+
+  const coordinate = [1, 2] satisfies Coordinate;
+  const res = getCharacter(coordinate, matrix);
+  assertEquals(res, null);
+});
+
+Deno.test('Solve Part 1 - hasXmasWord - valid', () => {
+  const matrix = [
+    'XMAS',
+  ];
+
+  const coordinate = [0, 0] satisfies Coordinate;
+  const coordinateGroup = [[0, 0], [1, 0], [2, 0], [3, 0]] satisfies CoordinateGroup;
+  const res = hasXmasWord(matrix, coordinate, coordinateGroup);
+  assertEquals(res, true);
+});
+
+Deno.test('Solve Part 1 - hasXmasWord - coordinate invalid', () => {
+  const matrix = [
+    'XMAS',
+  ];
+
+  const coordinate = [1, 0] satisfies Coordinate;
+  const coordinateGroup = [[0, 0], [1, 0], [2, 0], [3, 0]] satisfies CoordinateGroup;
+  const res = hasXmasWord(matrix, coordinate, coordinateGroup);
+  assertEquals(res, false);
+});
+
+Deno.test('Solve Part 1 - hasXmasWord - word invalid', () => {
+  const matrix = [
+    'XMAX',
+  ];
+
+  const coordinate = [0, 0] satisfies Coordinate;
+  const coordinateGroup = [[0, 0], [1, 0], [2, 0], [3, 0]] satisfies CoordinateGroup;
+  const res = hasXmasWord(matrix, coordinate, coordinateGroup);
+  assertEquals(res, false);
+});
+
+Deno.test('Solve Part 1 - Simple', () => {
+  const testValue = [
+    '....XXMAS.',
+    '.SAMXMS...',
+    '...S..A...',
+    '..A.A.MS.X',
+    'XMASAMX.MM',
+    'X.....XA.A',
+    'S.S.S.S.SS',
+    '.A.A.A.A.A',
+    '..M.M.M.MM',
+    '.X.X.XMASX',
+  ];
+
+  const res = solvePart1(testValue);
+  assertEquals(res, 18);
+});
---
+++ src/day-04.ts
@@ -0,0 +1,99 @@
+import { readLines } from './utils.ts';
+
+if (import.meta.main) {
+  init()
+    .catch(console.error);
+}
+
+async function init() {
+  const lines = await readLines(import.meta.filename!);
+  const part1 = solvePart1(lines);
+  const part2 = solvePart2(lines);
+
+  console.log(`Part 1 : ${part1}`);
+  console.log(`Part 2 : ${part2}`);
+}
+
+export type Coordinate = [number, number]; // [x, y]
+export type CoordinateGroup = Coordinate[]; // [[x1, y1], [x2, y2], ...]
+
+function getPossibleCoordinateGroupList(): CoordinateGroup[] {
+  return [
+    [[0, 0], [1, 0], [2, 0], [3, 0]], // Horizontally to the right
+    [[0, 0], [-1, 0], [-2, 0], [-3, 0]], // Horizontally to the left
+    [[0, 0], [0, 1], [0, 2], [0, 3]], // Vertically to the bottom
+    [[0, 0], [0, -1], [0, -2], [0, -3]], // Vertically to the top
+    [[0, 0], [1, -1], [2, -2], [3, -3]], // Diagonally to the right top
+    [[0, 0], [-1, -1], [-2, -2], [-3, -3]], // Diagonally to the left top
+    [[0, 0], [1, 1], [2, 2], [3, 3]], // Diagonally to the right bottom
+    [[0, 0], [-1, 1], [-2, 2], [-3, 3]], // Diagonally to the left bottom
+  ];
+}
+
+export function hasXmasWord(matrix: string[], currentCoordinate: Coordinate, givenCoordinateGroup: CoordinateGroup): boolean {
+  const expectedWord = 'XMAS';
+  for (let i = 0; i < expectedWord.length; i++) {
+    const expectedLetter = expectedWord[i];
+    const lookUpCoordinate = givenCoordinateGroup[i];
+    const coordinate = addUpCoordinates(currentCoordinate, lookUpCoordinate);
+    const letter = getCharacter(coordinate, matrix);
+    if (letter === null) {
+      return false;
+    }
+
+    if (letter !== expectedLetter) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export function getCharacter(coordinate: Coordinate, matrix: string[]): string | null {
+  if (!isCoordinateValid(coordinate, matrix)) {
+    return null;
+  }
+
+  return matrix[coordinate[1]][coordinate[0]];
+}
+
+export function addUpCoordinates(coordinate1: Coordinate, coordinate2: Coordinate): Coordinate {
+  const newX = coordinate1[0] + coordinate2[0];
+  const newY = coordinate1[1] + coordinate2[1];
+  return [newX, newY];
+}
+
+export function isCoordinateValid(coordinate: Coordinate, matrix: string[]): boolean {
+  const x = coordinate[0];
+  const y = coordinate[1];
+  if (x < 0 || y < 0) {
+    return false;
+  }
+
+  if (y > matrix.length - 1) {
+    return false;
+  }
+
+  if (x > matrix[0].length - 1) {
+    return false;
+  }
+
+  return true;
+}
+
+export function solvePart1(lines: string[]) {
+  const matrix = lines;
+  const coordinateGroupList = getPossibleCoordinateGroupList();
+
+  return matrix.map((line, y) => {
+    return [...line].map((_char, x) => {
+      const currentCoordinate = [x, y] satisfies Coordinate;
+      return coordinateGroupList.map((coordinateGroup) => hasXmasWord(matrix, currentCoordinate, coordinateGroup))
+        .reduce((acc, value) => value ? acc + 1 : acc, 0);
+    }).reduce((acc, value) => acc + value, 0);
+  }).reduce((acc, value) => acc + value, 0);
+}
+
+export function solvePart2(lines: string[]) {
+  return 0;
+}
